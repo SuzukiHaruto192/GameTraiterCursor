278439a5f7dbdca25d34cbe878986bdf8a715057
SuzukiHaruto192 <letamnatsu@gmail.com>
2026-03-11 23:14:41 +0700

    Add dash effect

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -10,10 +10,15 @@ public class Player : MonoBehaviour
     [SerializeField] private float jumpForce = 10f;
     [SerializeField] private float dashForce = 30f;
     [SerializeField] private float dashTime = 0.2f;
+    [SerializeField] private float animationDelay = 0.3f;
+    [SerializeField] private GameObject ghostPrefab;
+    [SerializeField] private float ghostDelay = 0.3f;
+    [SerializeField] private ParticleSystem windEffect;
 
     private bool isGrounded = true;
     private bool isReversed = false;
     private bool isDashing = false;
+    private bool hasDashedInAir = false;
 
     Rigidbody2D rb;
     Animator anim;   // Animator
@@ -32,6 +37,10 @@ public class Player : MonoBehaviour
     void Update()
     {
         if (isDashing) return;
+        if (isGrounded)
+        {
+            hasDashedInAir = false;
+        }
 
         float moveInput = Input.GetAxis("Horizontal");
 
@@ -60,24 +69,34 @@ public class Player : MonoBehaviour
             anim.SetBool("isIdle", true);
         }
 
-        // Nhảy
+        // Jump
         if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
         {
             rb.linearVelocity = new Vector2(rb.linearVelocity.x, jumpForce);
             isGrounded = false;
         }
+        if (rb.linearVelocity.y < 0)
+        {
+            rb.linearVelocity += Vector2.up * Physics2D.gravity.y * 1.5f * Time.deltaTime;            // Tăng trọng lực để rơi nhanh hơn
+        }
 
         // Attack
-        if (Input.GetKeyDown(KeyCode.J))
+        if (Input.GetKeyDown(KeyCode.X))
         {
             StartCoroutine(Attack());
         }
 
         // Dash
-        if (Input.GetKeyDown(KeyCode.C))
+        if (Input.GetKeyDown(KeyCode.C) && !hasDashedInAir)
         {
             StartCoroutine(Dash());
         }
+    }
+
+    private void LateUpdate()
+    {
+        float clampX = Math.Clamp(transform.position.x, minX, maxX);
+        transform.position = new Vector3(clampX, transform.position.y);
     }
 
     private void OnCollisionEnter2D(Collision2D collision)
@@ -90,15 +109,25 @@ public class Player : MonoBehaviour
 
     IEnumerator Dash()
     {
+        if (!isGrounded)
+        {
+            hasDashedInAir = true;
+        }
         isDashing = true;
         float originalGravity = rb.gravityScale;
         rb.gravityScale = 0;
+        rb.linearVelocity = new Vector2(transform.position.x, 0);
+        windEffect.Play();
 
         float dashDirection = isReversed ? -1 : 1;
         rb.linearVelocity = new Vector2(dashDirection * dashForce, rb.linearVelocity.y);
 
+        Coroutine ghostCoroutine = StartCoroutine(CreateGhost());
+
         yield return new WaitForSeconds(dashTime);
 
+        windEffect.Stop();
+        StopCoroutine(ghostCoroutine);
         rb.linearVelocity = Vector2.zero;
         rb.gravityScale = originalGravity;
         isDashing = false;
@@ -110,9 +139,23 @@ public class Player : MonoBehaviour
         anim.SetBool("isIdle", false);
         anim.SetBool("isWalk", false);
 
-        yield return new WaitForSeconds(0.3f); // thời gian animation
+        yield return new WaitForSeconds(animationDelay);
 
         anim.SetBool("isAttack", false);
         anim.SetBool("isIdle", true);
+    }
+
+    IEnumerator CreateGhost()
+    {
+        GameObject ghost = Instantiate(ghostPrefab, transform.position, transform.rotation);
+        SpriteRenderer playerSR = GetComponent<SpriteRenderer>();
+
+        while (true)
+        {
+            GhostEffect ghostEffect = ghost.GetComponent<GhostEffect>();
+            ghostEffect.SetGhost(playerSR.sprite, playerSR.flipX, playerSR.transform.localScale);
+
+            yield return new WaitForSeconds(ghostDelay);
+        }
     }
 }
