f006ff1b4c4d2a2ac735b74e2b2922988440a042
SuzukiHaruto192 <letamnatsu@gmail.com>
2026-03-13 12:21:16 +0700

    Add light effect

---
+++ Assets/Scripts/Environment/LightFlicker.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using UnityEngine.Rendering.Universal;
+
+public class LightFlicker : MonoBehaviour
+{
+    private Light2D myLight;
+    void Start() => myLight = GetComponent<Light2D>();
+
+    void Update()
+    {
+        // Làm ánh sáng nhấp nháy nhẹ nhàng
+        myLight.intensity = Mathf.PingPong(Time.time * 2f, 2.8f) + 0.2f;
+    }
+}
