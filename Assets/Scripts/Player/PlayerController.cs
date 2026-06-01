cd3bb3ccf5b857b5cbc635a85f1495031f223cb1
sangtran <stran3328@gmail.com>
2026-06-01 17:19:37 +0700

    chỉnh UI thanh HP trong base

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -1,5 +1,5 @@
 ﻿using UnityEngine;
-using System.Collections; // Bắt buộc phải có dòng này để dùng IEnumerator
+using System.Collections;
 using UnityEngine.SceneManagement;
 
 public class PlayerController : MonoBehaviour
